54ef0083e8e75cebc7c631688258b34152466b4a
林亮 <8199705@qq.com>
2017-07-25 21:29:39 +0800

    test script

--- src/test/ThreadedClientTester.java
+++ src/test/ThreadedClientTester.java
@@ -8,6 +8,27 @@ import com.xxdb.client.ThreadedClient;
 public class ThreadedClientTester {
 	public static void main(String args[]) {
 		
+		/*
+		 
+		//============ publish create table =============
+        n=20000000
+        t=table(n:0,`time`count`qty`price`exch,[TIMESTAMP,INT,INT,DOUBLE,INT])
+        share t as trades
+        setStream(trades,true)
+        t=NULL;
+        //===========insert data
+        rows = 1
+        timev = take(now(), rows)
+        count = take(1, rows)
+        qtyv = take(112, rows)
+        pricev = take(53.75, rows)
+        exchv = take(2, rows)
+        for(x in 0:20000000){
+        insert into trades values(timev, count, qtyv, pricev, exchv)
+        }
+        insert into trades values(timev, count, take(-1, rows), pricev, exchv);
+		 */
+		
         ThreadedClient client = new ThreadedClient(8991);
         
         try {
