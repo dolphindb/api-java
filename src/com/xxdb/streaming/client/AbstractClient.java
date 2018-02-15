a3fc5352d9458fa7a2278180304e66a60f077ce7
Davis Zhou <davis.zhou@dolphindb.com>
2018-02-15 13:51:29 -0500

    reflect the change on getSubscriptionTopic function

--- src/com/xxdb/streaming/client/AbstractClient.java
+++ src/com/xxdb/streaming/client/AbstractClient.java
@@ -12,6 +12,7 @@ import com.xxdb.DBConnection;
 import com.xxdb.data.BasicInt;
 import com.xxdb.data.BasicLong;
 import com.xxdb.data.BasicString;
+import com.xxdb.data.BasicAnyVector;
 import com.xxdb.data.Entity;
 import com.xxdb.streaming.client.IMessage;
 
@@ -98,7 +99,7 @@ abstract class AbstractClient implements MessageDispatcher{
 		params.add(new BasicString(tableName));
 		
 		re = dbConn.run("getSubscriptionTopic", params);
-		topic = re.getString();
+		topic = ((BasicAnyVector)re).getEntity(0).getString();
 		BlockingQueue<List<IMessage>> queue = queueManager.addQueue(topic);
 		params.clear();
 		
