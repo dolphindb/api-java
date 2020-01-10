863283a74e88d3d485d3be6624b7929509114d79
Linl <liang.lin@dolphindb.com>
2020-01-10 09:43:58 +0800

    add UtilsTest

--- test/com/xxdb/data/UtilsTest.java
+++ test/com/xxdb/data/UtilsTest.java
@@ -14,7 +14,7 @@ public class UtilsTest {
         assertEquals(0, Utils.countMonths(0,1));
         assertEquals(0, Utils.countMonths(YearMonth.of(0,1)));
     }
-
+    @Test
     public void test_parseMonth(){
         //base month 0000.01
         assertEquals(2018, Utils.parseMonth(24226).getYear());
