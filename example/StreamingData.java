9387165a5c4f47929dc8114e039e026e62747440
Linl <liang.lin@dolphindb.com>
2020-02-26 18:29:08 +0800

    update

--- example/StreamingData.java
+++ example/StreamingData.java
@@ -16,52 +16,10 @@ public class StreamingData {
     public static char METHOD = 'P';
     public static Integer subscribePORT = 8892;
 
-    private BasicTable createBasicTable() {
-        List<String> colNames = new ArrayList<String>();
-        colNames.add("id");
-        colNames.add("time");
-        colNames.add("sym");
-        colNames.add("qty");
-        colNames.add("price");
-        List<Vector> cols = new ArrayList<Vector>() {
-        };
-        //id
-        int[] vid = new int[]{0, 1};
-        BasicIntVector bidv = new BasicIntVector(vid);
-        cols.add(bidv);
-        //time
-        int[] vtime = new int[]{Utils.countMilliseconds(16, 46, 05, 123), Utils.countMilliseconds(18, 32, 05, 321)};
-        BasicTimeVector btimev = new BasicTimeVector(vtime);
-        cols.add(btimev);
-        //sym
-        String[] vsymbol = new String[]{"GOOG", "MS"};
-        BasicStringVector bsymbolv = new BasicStringVector(vsymbol);
-        cols.add(bsymbolv);
-        //qty
-        int[] vint = new int[]{1000, 2000};
-        BasicIntVector bintv = new BasicIntVector(vint);
-        cols.add(bintv);
-        //price
-        double[] vdouble = new double[]{24546.54, 4356.23};
-        BasicDoubleVector bdoublev = new BasicDoubleVector(vdouble);
-        cols.add(bdoublev);
-        BasicTable t1 = new BasicTable(colNames, cols);
-        return t1;
-    }
-
     public void writeStreamTable() throws IOException {
-        BasicTable table1 = createBasicTable();
         conn.login("admin", "123456", false);
         conn.run("share streamTable(30000:0,`id`time`sym`qty`price,[INT,TIME,SYMBOL,INT,DOUBLE]) as Trades\n");
         conn.run("def saveData(data){ Trades.tableInsert(data)}");
-        List<Entity> args = new ArrayList<Entity>(1);
-        args.add(table1);
-        conn.run("saveData", args);
-        BasicTable dt = (BasicTable) conn.run("select * from t");
-        System.out.println(dt.getString());
-        if (dt.rows() != 2) {
-            System.out.println("failed");
-        }
     }
 
     public void PollingClient() throws SocketException {
