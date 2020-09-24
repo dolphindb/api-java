4aaaf3cf82ec4330442f7e3a76aac711fe90e326
Linl <liang.lin@dolphindb.com>
2020-09-24 17:46:40 +0800

    update

--- src/com/xxdb/data/Chart.java
+++ src/com/xxdb/data/Chart.java
@@ -7,7 +7,7 @@ package com.xxdb.data;
  */
 
 public interface Chart extends Dictionary{
-	enum CHART_TYPE {CT_AREA, CT_BAR, CT_COLUMN, CT_HISTOGRAM, CT_LINE, CT_PIE, CT_SCATTER, CT_TREND};
+	enum CHART_TYPE {CT_AREA, CT_BAR, CT_COLUMN, CT_HISTOGRAM, CT_LINE, CT_PIE, CT_SCATTER, CT_TREND, CT_KLINE};
 	
 	CHART_TYPE getChartType() throws Exception;
 	Matrix getData() throws Exception;
