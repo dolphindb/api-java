d2c261e56f438e724054d2867d60f6b2376b1471
Xiaohua Zhou <davis.zhou@dolphindb.com>
2020-12-25 09:52:42 +0800

    support ValueDomain, ListDomain, and RangeDomain in DomainFactory

--- src/com/xxdb/route/DomainFactory.java
+++ src/com/xxdb/route/DomainFactory.java
@@ -3,6 +3,7 @@ package com.xxdb.route;
 import com.xxdb.data.BasicInt;
 import com.xxdb.data.Entity;
 import com.xxdb.data.Utils;
+import com.xxdb.data.Vector;
 
 public class DomainFactory {
     public static Domain createDomain(Entity.PARTITION_TYPE type, Entity.DATA_TYPE partitionColType, Entity values) throws Exception {
@@ -11,6 +12,21 @@ public class DomainFactory {
             Entity.DATA_CATEGORY dataCat = Utils.getCategory(dataType);
             int buckets = ((BasicInt)values).getInt();
             return new HashDomain(buckets, dataType, dataCat);
+        } 
+        else if (type == Entity.PARTITION_TYPE.VALUE) {
+            Entity.DATA_TYPE dataType = partitionColType;
+            Entity.DATA_CATEGORY dataCat = Utils.getCategory(dataType);
+              return new ValueDomain(dataType, dataCat);
+        } 
+        else if (type == Entity.PARTITION_TYPE.RANGE) {
+            Entity.DATA_TYPE dataType = partitionColType;
+            Entity.DATA_CATEGORY dataCat = Utils.getCategory(dataType);
+            return new RangeDomain((Vector)values, dataType, dataCat);
+        } 
+        else if (type == Entity.PARTITION_TYPE.LIST) {
+            Entity.DATA_TYPE dataType = partitionColType;
+            Entity.DATA_CATEGORY dataCat = Utils.getCategory(dataType);
+            return new ListDomain((Vector)values, dataType, dataCat);
         } 
         throw new RuntimeException("Unsupported partition type " + type.toString());
     }
