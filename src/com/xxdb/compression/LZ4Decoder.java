9b43f1c126ce3253d13a771706570c9ee4b8f234
Yitong Li <yitong.li@dolphindb.com>
2021-03-25 08:47:18 +0800

    basic decompression

--- src/com/xxdb/compression/LZ4Decoder.java
+++ src/com/xxdb/compression/LZ4Decoder.java
@@ -1,22 +1,44 @@
 package com.xxdb.compression;
+import com.xxdb.io.AbstractExtendedDataInputStream;
 import net.jpountz.lz4.LZ4Factory;
 import net.jpountz.lz4.LZ4FastDecompressor;
+import net.jpountz.lz4.LZ4SafeDecompressor;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class LZ4Decoder {
-    byte[] dest = null;
-    public LZ4Decoder(byte[] src, int blockSize, int dataSize) {
+    byte[] dest;
+
+    public LZ4Decoder(AbstractExtendedDataInputStream src, int srcSize, int dataSize, int unitLength) {
         LZ4Factory factory = LZ4Factory.fastestInstance();
-        LZ4FastDecompressor decompressor = factory.fastDecompressor();
-        dest = new byte[dataSize * Long.BYTES];
+        LZ4SafeDecompressor decompressor = factory.safeDecompressor();
+        dest = new byte[dataSize * unitLength];
+        int blockSize = 0;
         int count = 0;
-        while ((count + 1) * blockSize <= dataSize) {
-            decompressor.decompress(src, count * blockSize, dest, 20 + count * blockSize, blockSize); //FIXME: dest cannot directly use blockSize
-            count++;
+        int length = 0;
+        while (count < srcSize) {
+            try {
+                blockSize = src.readInt();
+//                System.out.println("blockSize: " + blockSize);
+                count+=Integer.BYTES;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (blockSize == 0) break;
+            blockSize = Math.min(blockSize, srcSize - count);
+            try {
+                byte[] srcArray = new byte[blockSize];
+                src.readFully(srcArray);
+                length+=decompressor.decompress(srcArray, 0, blockSize, dest, length);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            count+=blockSize;
         }
-        if (dataSize - count * blockSize > 0)
-            decompressor.decompress(src, count * blockSize, dest, 20 + count * blockSize, dataSize - count * blockSize);
+        System.out.println("block count: " + count);
     }
 
     public byte[] decompress() {
