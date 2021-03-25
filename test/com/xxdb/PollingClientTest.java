4d6a8e907841b36df436277151a0c625fefa4f27
noraXu <yanan.xu@dolphindb.com>
2021-03-25 12:05:16 +0800

    Create poolingclientTest

---
+++ test/com/xxdb/PollingClientTest.java
@@ -0,0 +1,179 @@
+/* ==============server publish data sample========== 
+   n=20000000
+   t=table(n:0,`time`sym`qty`price`exch`index,[TIMESTAMP,SYMBOL,INT,DOUBLE,SYMBOL,LONG])
+   share t as trades
+   setStream(trades,true)
+   t=NULL
+   rows = 1
+   timev = take(now(), rows)
+   symv = take(`MKFT, rows)
+   qtyv = take(112, rows)
+   pricev = take(53.75, rows)
+   exchv = take(`N, rows)
+   for(x in 0:2000000){
+   insert into trades values(timev, symv, qtyv, pricev, exchv,x)
+   }
+   insert into trades values(timev, symv, take(-1, 1), pricev, exchv,x)
+ */
+package com.xxdb;
+
+import com.xxdb.data.BasicByte;
+import com.xxdb.data.BasicInt;
+import com.xxdb.data.BasicTable;
+import com.xxdb.streaming.client.IMessage;
+import com.xxdb.streaming.client.PollingClient;
+import com.xxdb.streaming.client.TopicPoller;
+import org.junit.*;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PollingClientTest {
+    public static DBConnection conn ;
+    public static String HOST = "192.168.1.132";
+    public static Integer PORT = 8848;
+    public static PollingClient client;
+    @BeforeClass
+    public static void set() throws IOException {
+        conn = new DBConnection();
+        try {
+            if (!conn.connect(HOST, PORT, "admin", "123456")) {
+                throw new IOException("Failed to connect to 2xdb server");
+            }
+            client = new PollingClient(HOST,8676);
+            conn.run("st2 = streamTable(1000000:0,`tag`ts`data,[INT,TIMESTAMP,DOUBLE])\n" +
+                    "enableTableShareAndPersistence(table=st2, tableName=`Trades, asynWrite=true, compress=true, cacheSize=20000, retentionMinutes=180)\t\n");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @After
+    public  void after() throws IOException {
+        client.unsubscribe(HOST, 8848, "Trades", "subtrades");
+    }
+
+    @AfterClass
+    public static void cls(){
+        conn.close();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public  void error_size1() throws IOException {
+        TopicPoller poller1 = client.subscribe(HOST, 8848, "Trades","subtrades",-1,true);
+        ArrayList<IMessage> msgs;
+        msgs = poller1.poll(1000,0);
+       }
+    @Test(expected = RuntimeException.class)
+    public  void error_size2() throws IOException {
+        TopicPoller poller1 = client.subscribe(HOST, 8848, "Trades","subtrades",-1,true);
+        ArrayList<IMessage> msgs = poller1.poll(1000,-10);
+    }
+
+    @Test
+    public  void test_size() throws IOException {
+
+        TopicPoller poller1 = client.subscribe(HOST, 8848, "Trades","subtrades",-1,true);
+        ArrayList<IMessage> msgs;
+        try {
+            for (int i=0;i<10;i++){//data<size
+                conn.run("n=50;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(100,1000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    assertEquals(50, msgs.size());
+                }
+            }
+            for (int i=0;i<10;i++){//data>size
+                conn.run("n=5000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(100000,1000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    BasicInt value = (BasicInt) msgs.get(0).getEntity(0);
+                    assertTrue(msgs.size()>=1000);
+                }
+            }
+            for (int i=0;i<10;i++){//data=size
+                conn.run("n=5000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(1000000,5000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    BasicInt value = (BasicInt) msgs.get(0).getEntity(0);
+                    assertTrue(msgs.size()>=1000);
+                }
+            }
+            for (int i=0;i<10;i++){//bigsize
+                conn.run("n=5000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(100,1000000000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    BasicInt value = (BasicInt) msgs.get(0).getEntity(0);
+                    assertTrue(msgs.size()>=1000);
+                }
+            }
+            for (int i=0;i<2;i++){//bigdata
+                conn.run("n=10000000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(100,10000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    BasicInt value = (BasicInt) msgs.get(0).getEntity(0);
+                    assertTrue(msgs.size()>=10000);
+                }
+            }
+            for (int i=0;i<10;i++){//smalldata
+                conn.run("n=1;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(100,10000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    BasicInt value = (BasicInt) msgs.get(0).getEntity(0);
+                    assertTrue(msgs.size()>=1);
+                }
+            }
+            for (int i=0;i<10;i++){//append Many times
+                conn.run("n=10;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                conn.run("n=20;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                conn.run("n=30;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                conn.run("n=40;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs = poller1.poll(100,10000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                    BasicInt value = (BasicInt) msgs.get(0).getEntity(0);
+                    assertTrue(msgs.size()>=100);
+                }
+            }
+            for (int i=0;i<10;i++){//no data
+                msgs = poller1.poll(100,1000);
+                if (msgs==null){
+                    continue;
+                }
+                else if (msgs.size() > 0) {
+                   // System.out.println(msgs.size());
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
