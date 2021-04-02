c8c7b5169a2194a875e514774a2811aecea6b6c9
noraXu <yanan.xu@dolphindb.com>
2021-04-02 12:17:08 +0800

    Create PollingClientTest

--- test/com/xxdb/PollingClientTest.java
+++ test/com/xxdb/PollingClientTest.java
@@ -54,13 +54,27 @@ public class PollingClientTest {
 
     @After
     public  void after() throws IOException {
-        client.unsubscribe(HOST, 8848, "Trades", "subtrades");
+        try {
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades");
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades1");
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades2");
+            client.unsubscribe(HOST, 8848, "Trades");
+        }catch (Exception e){
+
+        }
     }
 
     @AfterClass
     public static void cls() throws IOException {
-        conn.run("dropStreamTable(`Trades)");
-        conn.close();
+        try {
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades");
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades1");
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades2");
+            client.unsubscribe(HOST, 8848, "Trades");
+            conn.run("dropStreamTable(`Trades)");
+            conn.close();
+        }catch (Exception e){
+        }
     }
 
     @Test(expected = RuntimeException.class)
@@ -115,7 +129,7 @@ public class PollingClientTest {
             }
             for (int i=0;i<10;i++){//bigsize
                 conn.run("n=5000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
-                msgs = poller1.poll(100,1000000000);
+                msgs = poller1.poll(1000,1000000000);
                 if (msgs==null){
                     continue;
                 }
@@ -126,7 +140,7 @@ public class PollingClientTest {
             }
             for (int i=0;i<2;i++){//bigdata
                 conn.run("n=10000000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
-                msgs = poller1.poll(100,10000);
+                msgs = poller1.poll(1000,10000);
                 if (msgs==null){
                     continue;
                 }
@@ -137,7 +151,7 @@ public class PollingClientTest {
             }
             for (int i=0;i<10;i++){//smalldata
                 conn.run("n=1;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
-                msgs = poller1.poll(100,10000);
+                msgs = poller1.poll(1000,10000);
                 if (msgs==null){
                     continue;
                 }
@@ -151,7 +165,7 @@ public class PollingClientTest {
                 conn.run("n=20;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
                 conn.run("n=30;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
                 conn.run("n=40;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
-                msgs = poller1.poll(100,10000);
+                msgs = poller1.poll(1000,10000);
                 if (msgs==null){
                     continue;
                 }
@@ -175,6 +189,39 @@ public class PollingClientTest {
         }
     }
 
+    @Test
+    public  void test_size_resubscribe() throws IOException {
+        for (int j=0;j<10;j++) {
+            TopicPoller poller1 = client.subscribe(HOST, 8848, "Trades", "subtrades1", -1, true);
+            TopicPoller poller2 = client.subscribe(HOST, 8848, "Trades", "subtrades2", -1, true);
+            ArrayList<IMessage> msgs1;
+            ArrayList<IMessage> msgs2;
+            for (int i = 0; i < 10; i++) {
+                conn.run("n=5000;t=table(1..n as tag,now()+1..n as ts,rand(100.0,n) as data);" + "Trades.append!(t)");
+                msgs1 = poller1.poll(100, 10000);
+                msgs2 = poller2.poll(100, 1000);
+                if (msgs1 == null) {
+                    continue;
+                } else if (msgs1.size() > 0) {
+                    BasicTable t = (BasicTable) conn.run("t");
+                    assertEquals(5000, msgs1.size());
+                  for (int k=0;k<msgs1.size();k++){
+                      assertEquals(t.getColumn(0).get(k),msgs1.get(k).getEntity(0));
+                      assertEquals(t.getColumn(1).get(k),msgs1.get(k).getEntity(1));
+                      assertEquals(t.getColumn(2).get(k),msgs1.get(k).getEntity(2));
+                  }
 
+                }
+                if (msgs2 == null) {
+                    continue;
+                } else if (msgs2.size() > 0) {
+                    BasicInt value = (BasicInt) msgs2.get(0).getEntity(0);
+                    assertTrue(msgs2.size() >= 1000);
+                }
 
+            }
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades1");
+            client.unsubscribe(HOST, 8848, "Trades", "subtrades2");
+        }
+    }
 }
