df7ce494008990d024c09a1957ad41669889c898
LAPTOP-AMITIFHM\llin <windancer_lin@163.com>
2021-09-29 00:49:37 +0800

    add hang case

--- test/com/xxdb/data/BasicArrayVectorTest.java
+++ test/com/xxdb/data/BasicArrayVectorTest.java
@@ -17,4 +17,16 @@ public class BasicArrayVectorTest {
                  "\ta.append!([1..100])\n" +
                  "};a");
     }
+
+    @Test
+    public void TestBasicIntArrayVectorHang() throws Exception {
+        DBConnection conn = new DBConnection();
+        conn.connect("localhost", 8848);
+        Entity obj = conn.run("a = array(INT[], 0, 10000)\n" +
+                "for(i in 1..10000){\n" +
+                "\tx = int()\n" +
+                "\ta.append!(x)\n" +
+                "}\n" +
+                "a");
+    }
 }
