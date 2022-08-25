92d83df4320bb6fab34c647f68ea1d3121acdf35
yi.shi <yi.shi@dolphindb.com>
2022-08-25 15:48:15 +0800

    DGUI-70: Fix decimal display bug

--- src/com/xxdb/data/BasicDecimal64.java
+++ src/com/xxdb/data/BasicDecimal64.java
@@ -4,6 +4,8 @@ import com.xxdb.io.ExtendedDataInput;
 import com.xxdb.io.ExtendedDataOutput;
 
 import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.time.temporal.Temporal;
 
 public class BasicDecimal64 extends AbstractScalar implements Comparable<BasicDecimal32>{
@@ -48,18 +50,13 @@ public class BasicDecimal64 extends AbstractScalar implements Comparable<BasicDe
             return "";
         else {
             StringBuilder sb = new StringBuilder();
-            sb.append((long)(value_ / Math.pow(10, scale_)));
-            int sign = value_ < 0? -1 : 1;
-            double data = value_ % Math.pow(10, scale_) * sign;
-            sb.append(".");
-            if (data == 0){
-                for (int i = 0; i < scale_;i++){
-                    sb.append("0");
-                }
-            }else {
-                sb.append((int)data);
+            sb.append("0.");
+            for (int i = 0; i < scale_; i++){
+                sb.append("#");
             }
-            return sb.toString();
+            DecimalFormat df = new DecimalFormat(sb.toString());
+            df.setRoundingMode(RoundingMode.FLOOR);
+            return df.format((double) value_ / Math.pow(10, scale_));
         }
     }
 
