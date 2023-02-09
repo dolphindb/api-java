b0bc7804c852060581154aa6995bdd1272177620
minxing.zou <minxing.zou@dolphindb.com>
2023-02-09 16:06:58 +0800

    AJ-255: fix bug of compress

--- src/com/xxdb/compression/AbstractDecoder.java
+++ src/com/xxdb/compression/AbstractDecoder.java
@@ -9,7 +9,7 @@ import java.nio.ByteOrder;
 public abstract class AbstractDecoder implements Decoder {
 
 	protected ByteBuffer createColumnVector(int rows, int unitLength, boolean isLittleEndian, int minSize, int extra, int type, short scale){
-		ByteBuffer out = ByteBuffer.allocate(Math.max(rows * unitLength + 12, minSize)).order(isLittleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+		ByteBuffer out = ByteBuffer.allocate(Math.max(rows * unitLength + 16, minSize)).order(isLittleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
 		out.putInt(rows);
 		out.putInt(extra);
 		Entity.DATA_TYPE dataType = Entity.DATA_TYPE.valueOf(type);
@@ -20,11 +20,13 @@ public abstract class AbstractDecoder implements Decoder {
 	}
 
 	protected ByteBuffer createLZ4ColumnVector(int rows, boolean isLittleEndian, int extra, int type, short scale){
-		ByteBuffer out = ByteBuffer.allocate(12).order(isLittleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+		Entity.DATA_TYPE dataType = Entity.DATA_TYPE.valueOf(type);
+		boolean decimalType = Utils.getCategory(dataType) == Entity.DATA_CATEGORY.DENARY || dataType == Entity.DATA_TYPE.DT_DECIMAL32_ARRAY || dataType == Entity.DATA_TYPE.DT_DECIMAL64_ARRAY || dataType == Entity.DATA_TYPE.DT_DECIMAL128_ARRAY;
+		int size = decimalType ? 12 : 8;
+		ByteBuffer out = ByteBuffer.allocate(size).order(isLittleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
 		out.putInt(rows);
 		out.putInt(extra);
-		Entity.DATA_TYPE dataType = Entity.DATA_TYPE.valueOf(type);
-		if(Utils.getCategory(dataType) == Entity.DATA_CATEGORY.DENARY || dataType == Entity.DATA_TYPE.DT_DECIMAL32_ARRAY || dataType == Entity.DATA_TYPE.DT_DECIMAL64_ARRAY || dataType == Entity.DATA_TYPE.DT_DECIMAL128_ARRAY){
+		if(decimalType){
 			out.putInt(scale);
 		}
 		return out;
