310baabe4f4e678531b78ebc18af9f8d73a34fc0
chengyitian <sylvestercheng@aliyun.com>
2023-10-31 14:06:56 +0800

    AJ-461: check if newColName is null or empty; check if newColName already exit in table.

--- src/com/xxdb/data/BasicTable.java
+++ src/com/xxdb/data/BasicTable.java
@@ -129,14 +129,17 @@ public class BasicTable extends AbstractEntity implements Table{
 	 * @param newColName
 	 */
 	public void replaceColName(String originalColName, String newColName) {
-		if (Utils.isEmpty(originalColName))
-			throw new RuntimeException("colName cannot be null.");
+		if (Objects.isNull(newColName) || Utils.isEmpty(newColName))
+			throw new RuntimeException("The param 'newColName' cannot be null or empty.");
+
+		if (this.colNames.contains(newColName))
+			throw new RuntimeException("The newColName '" + newColName +"' already exists in table. Column names cannot be duplicated.");
 
 		if (this.colNames.contains(originalColName)) {
 			int index = colNames.indexOf(originalColName);
 			colNames.set(index, newColName);
 		} else {
-			throw new RuntimeException("colName '" + originalColName +"' does not exist in table.");
+			throw new RuntimeException("The param originalColName '" + originalColName +"' does not exist in table.");
 		}
 	}
     
