039486edaf7d056b51c1b4d5b0fc445356b2b271
Wei Sun <wei.sun@dolphindb.com>
2024-01-16 18:18:04 +0800

    AJ-543:add test case about Duration

--- test/com/xxdb/data/BasicDurationTest.java
+++ test/com/xxdb/data/BasicDurationTest.java
@@ -1,6 +1,7 @@
 package com.xxdb.data;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.xxdb.DBConnection;
 import com.xxdb.io.Double2;
 import com.xxdb.io.ExtendedDataOutput;
 import com.xxdb.io.Long2;
@@ -11,9 +12,13 @@ import java.nio.ByteBuffer;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.ResourceBundle;
 
 import static org.junit.Assert.*;
 public class BasicDurationTest {
+    static ResourceBundle bundle = ResourceBundle.getBundle("com/xxdb/setup/settings");
+    static String HOST = bundle.getString("HOST");
+    static int PORT = Integer.parseInt(bundle.getString("PORT"));
     @Test
     public void test_BasicDuration(){
         BasicDuration bd = new BasicDuration(Entity.DURATION.MS,2);
@@ -249,8 +254,15 @@ public class BasicDurationTest {
         bdv.Append(bdv2);
         System.out.println(bdv.getString());
         assertEquals(size+4,bdv.rows());
-
     }
+//    @Test
+//    public void test_BasicDurationVector_Append_1() throws Exception {
+//        BasicDurationVector bdv = new BasicDurationVector(0);
+//        BasicDuration bdv2 = new BasicDuration(Entity.DURATION.MONTH,6);
+//        bdv2.setNull();
+//        bdv.Append(bdv2);
+//        System.out.println(bdv.getString());
+//    }
 
     @Test
     public void test_BasicDuration_serialize() throws Exception {
@@ -452,5 +464,86 @@ public class BasicDurationTest {
         String re = JSONObject.toJSONString(bd);
         System.out.println(re);
         assertEquals("{\"chart\":false,\"chunk\":false,\"dataCategory\":\"SYSTEM\",\"dataForm\":\"DF_SCALAR\",\"dataType\":\"DT_DURATION\",\"dictionary\":false,\"duration\":2,\"jsonString\":\"2ms\",\"matrix\":false,\"null\":false,\"number\":2,\"pair\":false,\"scalar\":true,\"string\":\"2ms\",\"table\":false,\"unit\":\"MS\",\"vector\":false}", re);
+    }
+    @Test
+    public void test_BasicDuration_1() throws IOException {
+        DBConnection conn = new DBConnection();
+        conn.connect(HOST,PORT,"admin","123456");
+        BasicDuration bd = (BasicDuration)conn.run("duration(3XNYS)");
+        assertEquals(3,bd.getDuration());
+        assertEquals("3XNYS",bd.getString());
+        BasicDuration bd1 = (BasicDuration)conn.run("duration(-3XNYS)");
+        assertEquals(-3,bd1.getDuration());
+        assertEquals("-3XNYS",bd1.getString());
+        BasicDuration bd2 = (BasicDuration)conn.run("duration(0XNYS)");
+        assertEquals(0,bd2.getDuration());
+        assertEquals("0XNYS",bd2.getString());
+        BasicDuration bd3 = (BasicDuration)conn.run("duration(-9999999XNYS)");
+        assertEquals(-9999999,bd3.getDuration());
+        assertEquals("-9999999XNYS",bd3.getString());
+    }
+    @Test
+    public void test_BasicDuration_2() throws IOException {
+        BasicDuration bd = new BasicDuration("XNYS",3);
+        assertEquals(3,bd.getDuration());
+        assertEquals("3XNYS",bd.getString());
+        BasicDuration bd1 = new BasicDuration("XNYS",-300);
+        assertEquals(-300,bd1.getDuration());
+        assertEquals("-300XNYS",bd1.getString());
+        BasicDuration bd2 = new BasicDuration("XNYS",0);
+        assertEquals(0,bd2.getDuration());
+        assertEquals("0XNYS",bd2.getString());
+        BasicDuration bd3 = new BasicDuration("XNYS",-9999999);
+        assertEquals(-9999999,bd3.getDuration());
+        assertEquals("-9999999XNYS",bd3.getString());
+        String ex = null;
+        try{
+            BasicDuration bd4 = new BasicDuration("1111",-9999999);
+        }catch(Exception e){
+            ex = e.getMessage();
+        }
+        assertEquals("The value of unit must duration enum type or contain four consecutive uppercase letters.",ex);
+        try{
+            BasicDuration bd4 = new BasicDuration("aaaa",-9999999);
+        }catch(Exception e){
+            ex = e.getMessage();
+        }
+        assertEquals("The value of unit must duration enum type or contain four consecutive uppercase letters.",ex);
+        try{
+            BasicDuration bd4 = new BasicDuration("aaaaaa",-9999999);
+        }catch(Exception e){
+            ex = e.getMessage();
+        }
+        assertEquals("The value of unit must duration enum type or contain four consecutive uppercase letters.",ex);
+
+    }
+    @Test
+    public void test_BasicDuration_not_support() throws IOException {
+        String ex = null;
+        try{
+            BasicDuration bd = new BasicDuration(Entity.DURATION.TDAY,3);
+        }catch(Exception e){
+            ex = e.getMessage();
+        }
+        assertEquals("the exchange unit should be given as String when use exchange calendar.",ex);
+    }
+    @Test
+    public void test_BasicDurationVector_1() throws Exception {
+        DBConnection conn = new DBConnection();
+        conn.connect(HOST,PORT,"admin","123456");
+        BasicAnyVector bdv = (BasicAnyVector)conn.run("[duration(3XNYS),duration(3XNYS)]");
+        System.out.println(bdv.getString());
+        assertEquals("(3XNYS,3XNYS)",bdv.getString());
+        System.out.println(((BasicDuration)bdv.get(0)).getDuration());
+        assertEquals(3,((BasicDuration)bdv.get(0)).getDuration());
+    }
+    @Test
+    public void test_BasicDurationVector_2() throws Exception {
+        BasicDurationVector bdv = new BasicDurationVector(2);
+        bdv.set(0,new BasicDuration("XNYS",3));
+        bdv.set(1,new BasicDuration("XNYS",0));
+        bdv.Append(new BasicDuration("XNYS",-999999999));
+        assertEquals("[3XNYS,0XNYS,-999999999XNYS]",bdv.getString());
+        assertEquals("3XNYS",bdv.get(0).getString());
     }
 }
