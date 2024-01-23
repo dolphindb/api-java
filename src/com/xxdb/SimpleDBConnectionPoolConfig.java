3c9afdd055e8ae084b139475a4172c10d10adc01
lucaijun <caijun.lu@dolphindb.com>
2024-01-23 10:27:44 +0800

    AJ-551: throw error when invalid hostname

--- src/com/xxdb/SimpleDBConnectionPoolConfig.java
+++ src/com/xxdb/SimpleDBConnectionPoolConfig.java
@@ -1,6 +1,5 @@
 package com.xxdb;
 
-import org.apache.commons.validator.routines.InetAddressValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.net.util.IPAddressUtil;
