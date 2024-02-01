287743f78bed6ff1697932ee3485d2a12c3faca9
chengyitian <sylvestercheng@aliyun.com>
2024-02-01 10:25:45 +0800

    AJ-567: delete com.xxdb.compression.LZ4Encoder#compress(java.nio.ByteBuffer, int, int, int);

--- src/com/xxdb/compression/LZ4Encoder.java
+++ src/com/xxdb/compression/LZ4Encoder.java
@@ -57,28 +57,4 @@ public class LZ4Encoder implements Encoder {
         }
         return out;
     }
-
-    public ByteBuffer compress(ByteBuffer in, int elementCount, int unitLength, int maxCompressedLength) throws IOException {
-        if(compressor == null){
-            LZ4Factory factory = LZ4Factory.fastestInstance();
-            compressor = factory.fastCompressor();
-        }
-        ByteBuffer out = ByteBuffer.allocate(maxCompressedLength);
-        int count = 0;
-        while (elementCount > 0 && count < maxCompressedLength) {
-            int blockSize = Math.min(DEFAULT_BLOCK_SIZE, elementCount * unitLength);
-            byte[] src = new byte[blockSize];
-            byte[] dest = new byte[blockSize];
-            in.get(src);
-            int compressedLength = compressor.compress(src, 0, blockSize, dest, 0);
-            //write
-            out.putInt(compressedLength);
-            out.put(dest, 0, compressedLength);
-            count+=Integer.BYTES;
-            count += compressedLength;
-            elementCount -= blockSize / unitLength;
-        }
-        out.flip();
-        return out;
-    }
 }
