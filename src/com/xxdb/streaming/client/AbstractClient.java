3829eabc8b8be0d8362a81cb7af85c574ac6ce73
chengyitian <sylvestercheng@aliyun.com>
2024-03-28 14:31:12 +0800

    AJ-656: modify response error info;

--- src/com/xxdb/streaming/client/AbstractClient.java
+++ src/com/xxdb/streaming/client/AbstractClient.java
@@ -578,7 +578,7 @@ public abstract class AbstractClient implements MessageDispatcher {
             int v1 = Integer.parseInt(_[1]);
             int v2 = Integer.parseInt(_[2]);
 
-            if (v0 ==3 || (v0 == 2 && v1 == 0 && v2 >= 9) || (v0 == 2 && v1 == 10) || (v0 == 3 && v1 == 0 && v2 >= 0)) {
+            if ((v0 == 2 && v1 == 0 && v2 >= 9) || (v0 == 2 && v1 == 10) || (v0 == 3 && v1 == 0 && v2 >= 0)) {
                 //server only support reverse connection
                 this.listeningPort = 0;
             } else {
