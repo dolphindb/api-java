fbea1ec16b6776959a3ae4852b009ad12e8045f3
chengyitian <sylvestercheng@aliyun.com>
2024-11-01 14:41:14 +0800

    update java api version to 3.00.2.1;

--- src/com/xxdb/data/Utils.java
+++ src/com/xxdb/data/Utils.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 
 public class Utils {
 
-	public static final String JAVA_API_VERSION = "3.00.2.0";
+	public static final String JAVA_API_VERSION = "3.00.2.1";
 
 	public static final int DISPLAY_ROWS = 20;
 	public static final int DISPLAY_COLS = 100;
