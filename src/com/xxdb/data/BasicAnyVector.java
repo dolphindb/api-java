420a73e46d7c910c213b75d568e3fcc56b711577
chengyitian <sylvestercheng@aliyun.com>
2025-08-27 15:44:43 +0800

    AJ-915: support Append、set, modify 'Entity[] values' to 'List<Entity> values', deprecated protected BasicAnyVector(Entity[] array, boolean copy);

--- src/com/xxdb/data/BasicAnyVector.java
+++ src/com/xxdb/data/BasicAnyVector.java
@@ -2,6 +2,9 @@ package com.xxdb.data;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.xxdb.io.ExtendedDataInput;
@@ -13,66 +16,72 @@ import com.xxdb.io.ExtendedDataOutput;
  *
  */
 
-public class BasicAnyVector extends AbstractVector{
-	private Entity[] values;
-	
-	public BasicAnyVector(int size){
+public class BasicAnyVector extends AbstractVector {
+	private List<Entity> values;
+
+	public BasicAnyVector(int size) {
 		super(DATA_FORM.DF_VECTOR);
-		values = new Entity[size];
-	}
-	
-	protected BasicAnyVector(Entity[] array, boolean copy){
-		super(DATA_FORM.DF_VECTOR);
-		if(copy)
-			values = array.clone();
-		else
-			values = array;
-	}
-	
-	protected BasicAnyVector(ExtendedDataInput in) throws IOException{
-		super(DATA_FORM.DF_VECTOR); 
-		int rows = in.readInt();
-		int cols = in.readInt();
-		values = new Entity[rows];
-		for (int i=0; i<rows; ++i) {
-			short flag = in.readShort();
-			int form = flag>>8;
-			int type = flag & 0xff;
-            boolean extended = type >= 128;
-            if(type >= 128)
-            	type -= 128;
-			Entity obj = BasicEntityFactory.instance().createEntity(DATA_FORM.values()[form], DATA_TYPE.valueOf(type), in, extended);
-			values[i] = obj;
+		values = new ArrayList<>(size);
+		for (int i = 0; i < size; ++i) {
+			values.add(null);
 		}
 	}
 
-	public Entity getEntity(int index){
-		return values[index];
+	@Deprecated
+	protected BasicAnyVector(Entity[] array, boolean copy) {
+		super(DATA_FORM.DF_VECTOR);
+		values = new ArrayList<>(Arrays.asList(array));
 	}
-	
-	public Entity get(int index){
-		return values[index];
+
+	protected BasicAnyVector(ExtendedDataInput in) throws IOException {
+		super(DATA_FORM.DF_VECTOR);
+		int rows = in.readInt();
+		int cols = in.readInt();
+		values = new ArrayList<>(rows);
+
+		for (int i = 0; i < rows; ++i) {
+			short flag = in.readShort();
+			int form = flag >> 8;
+			int type = flag & 0xff;
+			boolean extended = type >= 128;
+			if (type >= 128)
+				type -= 128;
+			Entity obj = BasicEntityFactory.instance().createEntity(DATA_FORM.values()[form], DATA_TYPE.valueOf(type), in, extended);
+			values.add(obj);
+		}
 	}
-	
-	public Vector getSubVector(int[] indices){
+
+	public Entity getEntity(int index) {
+		return values.get(index);
+	}
+
+	public Entity get(int index) {
+		return values.get(index);
+	}
+
+	public Vector getSubVector(int[] indices) {
 		int length = indices.length;
 		Entity[] sub = new Entity[length];
-		for(int i=0; i<length; ++i)
-			sub[i] = values[indices[i]];
+		for (int i = 0; i < length; ++i)
+			sub[i] = values.get(indices[i]);
 		return new BasicAnyVector(sub, false);
 	}
-	
+
 	public void set(int index, Entity value) throws Exception {
-		values[index] = value;
+		values.set(index, value);
 	}
 
 	@Override
 	public void set(int index, Object value) {
-		throw new RuntimeException("BasicAnyVector.set(int index, Object value) not supported.");
+		if (value instanceof Entity) {
+			values.set(index, (Entity) value);
+		} else {
+			throw new RuntimeException("BasicAnyVector.set(int index, Object value) only supports Entity objects.");
+		}
 	}
 
-	public void setEntity(int index, Entity value){
-		values[index] = value;
+	public void setEntity(int index, Entity value) {
+		values.set(index, value);
 	}
 
 	@Override
@@ -82,12 +91,12 @@ public class BasicAnyVector extends AbstractVector{
 
 	@Override
 	public boolean isNull(int index) {
-		return values[index] == null || (values[index].isScalar() && ((Scalar)values[index]).isNull());
+		return values.get(index) == null || (values.get(index).isScalar() && ((Scalar)values.get(index)).isNull());
 	}
 
 	@Override
 	public void setNull(int index) {
-		values[index] = new Void();
+		values.set(index, new Void());
 	}
 
 	@Override
@@ -102,12 +111,12 @@ public class BasicAnyVector extends AbstractVector{
 
 	@Override
 	public int rows() {
-		return values.length;
+		return values.size();
 	}
 
 	@JsonIgnore
 	@Override
-	public int getUnitLength(){
+	public int getUnitLength() {
 		throw new RuntimeException("BasicAnyVector.getUnitLength not supported.");
 	}
 
@@ -117,12 +126,12 @@ public class BasicAnyVector extends AbstractVector{
 
 	@Override
 	public void Append(Scalar value) {
-		throw new RuntimeException("AnyVector not support append");
+		values.add(value);
 	}
 
 	@Override
 	public void Append(Vector value) {
-		throw new RuntimeException("AnyVector not support append");
+		values.add(value);
 	}
 
 	@Override
@@ -130,16 +139,16 @@ public class BasicAnyVector extends AbstractVector{
 		throw new RuntimeException("BasicAnyVector not support checkCapacity.");
 	}
 
-	public String getString(){
+	public String getString() {
 		StringBuilder sb = new StringBuilder("(");
 		int size = Math.min(10, rows());
-		if(size > 0)
+		if (size > 0)
 			sb.append(getEntity(0).getString());
-		for(int i=1; i<size; ++i){
+		for (int i = 1; i < size; ++i) {
 			sb.append(',');
 			sb.append(getEntity(i).getString());
 		}
-		if(size < rows())
+		if (size < rows())
 			sb.append(",...");
 		sb.append(")");
 		return sb.toString();
@@ -155,7 +164,7 @@ public class BasicAnyVector extends AbstractVector{
 	}
 
 	@Override
-	public int serialize(int indexStart, int offect, int targetNumElement, AbstractVector.NumElementAndPartial numElementAndPartial, ByteBuffer out) throws IOException{
+	public int serialize(int indexStart, int offect, int targetNumElement, AbstractVector.NumElementAndPartial numElementAndPartial, ByteBuffer out) throws IOException {
 		throw new RuntimeException("BasicAnyVector.serialize not supported.");
 	}
 
@@ -165,13 +174,12 @@ public class BasicAnyVector extends AbstractVector{
 
 	@Override
 	protected void writeVectorToOutputStream(ExtendedDataOutput out) throws IOException {
-		for(Entity value : values) {
+		for (Entity value : values) {
 			if (Objects.nonNull(value))
 				value.write(out);
 		}
-
 	}
-	
+
 	@Override
 	public int asof(Scalar value) {
 		throw new RuntimeException("BasicAnyVector.asof not supported.");
