09f588f79934ac748cb57bd4ce5d2c2d202d7370
sunwei <wei.sun@dolphindb.com>
2025-11-20 18:10:42 +0800

    AJ-974:add test case about createScalar

--- test/com/xxdb/data/BasicEntityTest.java
+++ test/com/xxdb/data/BasicEntityTest.java
@@ -729,7 +729,232 @@ public class BasicEntityTest {
         BasicDecimal128  re2 = (BasicDecimal128)factory.createScalar(Entity.DATA_TYPE.DT_DECIMAL128, "21474836",2);
         assertEquals("21474836.00",re2.getString());
     }
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_bool() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        String ex = null;
+        BasicBoolean  re1 = (BasicBoolean)factory.createScalar(Entity.DATA_TYPE.DT_BOOL, "true",0);
+        assertEquals("true",re1.getString());
+        BasicBoolean  re2 = (BasicBoolean)factory.createScalar(Entity.DATA_TYPE.DT_BOOL, "false",0);
+        assertEquals("false",re2.getString());
 
+        BasicBoolean  re3 = (BasicBoolean)factory.createScalar(Entity.DATA_TYPE.DT_BOOL, "1",0);
+        assertEquals("true",re1.getString());
+        BasicBoolean  re4 = (BasicBoolean)factory.createScalar(Entity.DATA_TYPE.DT_BOOL, "0",0);
+        assertEquals("false",re2.getString());
+        BasicBoolean  re5 = (BasicBoolean)factory.createScalar(Entity.DATA_TYPE.DT_BOOL, null,0);
+        assertEquals("false",re2.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_byte() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        String ex = null;
+        BasicByte  re1 = (BasicByte)factory.createScalar(Entity.DATA_TYPE.DT_BYTE, "1",0);
+        assertEquals("1",re1.getString());
+        BasicByte  re2 = (BasicByte)factory.createScalar(Entity.DATA_TYPE.DT_BYTE, "12",0);
+        assertEquals("12",re2.getString());
+        BasicByte  re3 = (BasicByte)factory.createScalar(Entity.DATA_TYPE.DT_BYTE, null,0);
+        assertEquals("",re3.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_short() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        String ex = null;
+        BasicShort  re1 = (BasicShort)factory.createScalar(Entity.DATA_TYPE.DT_SHORT, "-32768",0);
+        assertEquals("",re1.getString());
+        BasicShort  re2 = (BasicShort)factory.createScalar(Entity.DATA_TYPE.DT_SHORT, "32767",0);
+        assertEquals("32767",re2.getString());
+        BasicShort  re3 = (BasicShort)factory.createScalar(Entity.DATA_TYPE.DT_SHORT, null,0);
+        assertEquals("",re3.getString());
+        BasicShort  re4 = (BasicShort)factory.createScalar(Entity.DATA_TYPE.DT_SHORT, "0",0);
+        assertEquals("0",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_int() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        String ex = null;
+        BasicInt  re1 = (BasicInt)factory.createScalar(Entity.DATA_TYPE.DT_INT, "-2147483648",0);
+        assertEquals("",re1.getString());
+        BasicInt  re2 = (BasicInt)factory.createScalar(Entity.DATA_TYPE.DT_INT, "2147483647",0);
+        assertEquals("2147483647",re2.getString());
+        BasicInt  re3 = (BasicInt)factory.createScalar(Entity.DATA_TYPE.DT_INT, null,0);
+        assertEquals("",re3.getString());
+        BasicInt  re4 = (BasicInt)factory.createScalar(Entity.DATA_TYPE.DT_INT, "0",0);
+        assertEquals("0",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_long() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        String ex = null;
+        BasicLong  re1 = (BasicLong)factory.createScalar(Entity.DATA_TYPE.DT_LONG, "-9223372036854775808",0);
+        assertEquals("",re1.getString());
+        BasicLong  re2 = (BasicLong)factory.createScalar(Entity.DATA_TYPE.DT_LONG, "9223372036854775807",0);
+        assertEquals("9223372036854775807",re2.getString());
+        BasicLong  re3 = (BasicLong)factory.createScalar(Entity.DATA_TYPE.DT_LONG, null,0);
+        assertEquals("",re3.getString());
+        BasicLong  re4 = (BasicLong)factory.createScalar(Entity.DATA_TYPE.DT_LONG, "0",0);
+        assertEquals("0",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_float() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        String ex = null;
+        BasicFloat  re1 = (BasicFloat)factory.createScalar(Entity.DATA_TYPE.DT_FLOAT, "-9.99",0);
+        assertEquals("-9.99",re1.getString());
+        BasicFloat  re2 = (BasicFloat)factory.createScalar(Entity.DATA_TYPE.DT_FLOAT, "922.3372",0);
+        assertEquals("922.3372",re2.getString());
+        BasicFloat  re3 = (BasicFloat)factory.createScalar(Entity.DATA_TYPE.DT_FLOAT, null,0);
+        assertEquals("",re3.getString());
+        BasicFloat  re4 = (BasicFloat)factory.createScalar(Entity.DATA_TYPE.DT_FLOAT, "0",0);
+        assertEquals("0",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_double() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicDouble  re1 = (BasicDouble)factory.createScalar(Entity.DATA_TYPE.DT_DOUBLE, "-9.99",0);
+        assertEquals("-9.99",re1.getString());
+        BasicDouble  re2 = (BasicDouble)factory.createScalar(Entity.DATA_TYPE.DT_DOUBLE, "922.3372",0);
+        assertEquals("922.3372",re2.getString());
+        BasicDouble  re3 = (BasicDouble)factory.createScalar(Entity.DATA_TYPE.DT_DOUBLE, null,0);
+        assertEquals("",re3.getString());
+        BasicDouble  re4 = (BasicDouble)factory.createScalar(Entity.DATA_TYPE.DT_DOUBLE, "0",0);
+        assertEquals("0",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_date() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicDate  re1 = (BasicDate)factory.createScalar(Entity.DATA_TYPE.DT_DATE, "2024-06-02",0);
+        assertEquals("2024.06.02",re1.getString());
+        BasicDate  re2 = (BasicDate)factory.createScalar(Entity.DATA_TYPE.DT_DATE, "1964-06-02",0);
+        assertEquals("1964.06.02",re2.getString());
+        BasicDate  re3 = (BasicDate)factory.createScalar(Entity.DATA_TYPE.DT_DATE, null,0);
+        assertEquals("",re3.getString());
+        BasicDate  re4 = (BasicDate)factory.createScalar(Entity.DATA_TYPE.DT_DATE, "2039-06-02",0);
+        assertEquals("2039.06.02",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_month() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicMonth  re1 = (BasicMonth)factory.createScalar(Entity.DATA_TYPE.DT_MONTH, "2024-06",0);
+        assertEquals("2024.06M",re1.getString());
+        BasicMonth  re2 = (BasicMonth)factory.createScalar(Entity.DATA_TYPE.DT_MONTH, "1964-06",0);
+        assertEquals("1964.06M",re2.getString());
+        BasicMonth  re3 = (BasicMonth)factory.createScalar(Entity.DATA_TYPE.DT_MONTH, null,0);
+        assertEquals("",re3.getString());
+        BasicMonth  re4 = (BasicMonth)factory.createScalar(Entity.DATA_TYPE.DT_MONTH, "2039-06",0);
+        assertEquals("2039.06M",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_time() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicTime  re1 = (BasicTime)factory.createScalar(Entity.DATA_TYPE.DT_TIME, "10:00:00.999",0);
+        assertEquals("10:00:00.999",re1.getString());
+        BasicTime  re2 = (BasicTime)factory.createScalar(Entity.DATA_TYPE.DT_TIME, "23:59:59.888",0);
+        assertEquals("23:59:59.888",re2.getString());
+        BasicTime  re3 = (BasicTime)factory.createScalar(Entity.DATA_TYPE.DT_TIME, null,0);
+        assertEquals("",re3.getString());
+        BasicTime  re4 = (BasicTime)factory.createScalar(Entity.DATA_TYPE.DT_TIME, "00:00:00.000",0);
+        assertEquals("00:00:00.000",re4.getString());
+    }
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_minute() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicMinute  re1 = (BasicMinute)factory.createScalar(Entity.DATA_TYPE.DT_MINUTE, "10:00:00",0);
+        assertEquals("10:00m",re1.getString());
+        BasicMinute  re2 = (BasicMinute)factory.createScalar(Entity.DATA_TYPE.DT_MINUTE, "23:59:59",0);
+        assertEquals("23:59m",re2.getString());
+        BasicMinute  re3 = (BasicMinute)factory.createScalar(Entity.DATA_TYPE.DT_MINUTE, null,0);
+        assertEquals("",re3.getString());
+        BasicMinute  re4 = (BasicMinute)factory.createScalar(Entity.DATA_TYPE.DT_MINUTE, "00:00:00",0);
+        assertEquals("00:00m",re4.getString());
+    }
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_second() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicSecond  re1 = (BasicSecond)factory.createScalar(Entity.DATA_TYPE.DT_SECOND, "10:00:00",0);
+        assertEquals("10:00:00",re1.getString());
+        BasicSecond  re2 = (BasicSecond)factory.createScalar(Entity.DATA_TYPE.DT_SECOND, "23:59:59",0);
+        assertEquals("23:59:59",re2.getString());
+        String obj = null;
+        BasicSecond  re3 = (BasicSecond)factory.createScalar(Entity.DATA_TYPE.DT_SECOND, obj,0);
+        assertEquals("",re3.getString());
+        BasicSecond  re4 = (BasicSecond)factory.createScalar(Entity.DATA_TYPE.DT_SECOND, "00:00:00",0);
+        assertEquals("00:00:00",re4.getString());
+    }
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_datetime() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicDateTime  re1 = (BasicDateTime)factory.createScalar(Entity.DATA_TYPE.DT_DATETIME, "2024-06-02T10:00:00.000",0);
+        assertEquals("2024.06.02T10:00:00",re1.getString());
+        BasicDateTime  re2 = (BasicDateTime)factory.createScalar(Entity.DATA_TYPE.DT_DATETIME, "1964-06-02T23:59:59.999",0);
+        assertEquals("1964.06.02T23:59:59",re2.getString());
+        BasicDateTime  re3 = (BasicDateTime)factory.createScalar(Entity.DATA_TYPE.DT_DATETIME, null,0);
+        assertEquals("",re3.getString());
+        BasicDateTime  re4 = (BasicDateTime)factory.createScalar(Entity.DATA_TYPE.DT_DATETIME, "2036-06-02T10:00:00.888",0);
+        assertEquals("2036.06.02T10:00:00",re4.getString());
+    }
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_timestamp() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicTimestamp  re1 = (BasicTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_TIMESTAMP, "2024-06-02T10:00:00.000",0);
+        assertEquals("2024.06.02T10:00:00.000",re1.getString());
+        BasicTimestamp  re2 = (BasicTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_TIMESTAMP, "1964-06-02T23:59:59.999",0);
+        assertEquals("1964.06.02T23:59:59.999",re2.getString());
+        BasicTimestamp  re3 = (BasicTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_TIMESTAMP, null,0);
+        assertEquals("",re3.getString());
+        BasicTimestamp  re4 = (BasicTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_TIMESTAMP, "2039-06-02T10:00:00.888",0);
+        assertEquals("2039.06.02T10:00:00.888",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_nanotime() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicNanoTime  re = (BasicNanoTime)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIME, "10:00:00.000",0);
+        assertEquals("10:00:00.000000000",re.getString());
+        BasicNanoTime  re1 = (BasicNanoTime)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIME, "2024-06-02T10:00:00.000",0);
+        assertEquals("2024.06.02 10:00:00.000",re1.getString());
+        BasicNanoTime  re2 = (BasicNanoTime)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIME, "1964-06-02T23:59:59.999",0);
+        assertEquals("1964.06.02 23:59:59.999",re2.getString());
+        BasicNanoTime  re3 = (BasicNanoTime)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIME, null,0);
+        assertEquals("",re3.getString());
+        BasicNanoTime  re4 = (BasicNanoTime)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIME, "2039-06-02T10:00:00.888",0);
+        assertEquals("2039.06.02 10:00:00.888",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_nanotimestamp() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicNanoTimestamp  re1 = (BasicNanoTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIMESTAMP, "2024-06-02T10:00:00.000",0);
+        assertEquals("2024.06.02T10:00:00.000000000",re1.getString());
+        BasicNanoTimestamp  re2 = (BasicNanoTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIMESTAMP, "1964-06-02T23:59:59.999",0);
+        assertEquals("1964.06.02T23:59:59.999000000",re2.getString());
+        BasicNanoTimestamp  re3 = (BasicNanoTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIMESTAMP, null,0);
+        assertEquals("",re3.getString());
+        BasicNanoTimestamp  re4 = (BasicNanoTimestamp)factory.createScalar(Entity.DATA_TYPE.DT_NANOTIMESTAMP, "2039-06-02T10:00:00.888",0);
+        assertEquals("2039.06.02T10:00:00.888000000",re4.getString());
+    }
+
+    @Test
+    public void test_BasicEntityFactory_createScalar_string_datehour() throws Exception {
+        BasicEntityFactory factory = new BasicEntityFactory();
+        BasicDateHour  re1 = (BasicDateHour)factory.createScalar(Entity.DATA_TYPE.DT_DATEHOUR, "2024-06-02T10:00:00.000",0);
+        assertEquals("2024.06.02T10",re1.getString());
+        BasicDateHour  re2 = (BasicDateHour)factory.createScalar(Entity.DATA_TYPE.DT_DATEHOUR, "1964-06-02T23:59:59.999",0);
+        assertEquals("1964.06.02T23",re2.getString());
+        BasicDateHour  re3 = (BasicDateHour)factory.createScalar(Entity.DATA_TYPE.DT_DATEHOUR, null,0);
+        assertEquals("",re3.getString());
+        BasicDateHour  re4 = (BasicDateHour)factory.createScalar(Entity.DATA_TYPE.DT_DATEHOUR, "2039-06-02T10:00:00.888",0);
+        assertEquals("2039.06.02T10",re4.getString());
+    }
     @Test
     public void test_BasicEntityFactory_createScalar_float() throws Exception {
         BasicEntityFactory factory = new BasicEntityFactory();
